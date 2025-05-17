ae3871f1b0ed6011df74de3fc66741e4d4d9b4e2
Mathis_D <mathis.d@acistec.fr>
2025-05-17 15:37:14 +0200

    LLS docs

--- netutils/LowLatSocket.h
+++ netutils/LowLatSocket.h
@@ -15,43 +15,93 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+/**
+ * @enum EthProtocol
+ * @brief Enum containing different values for the Ethernet protocol field, corresponding to different OAN stream types.
+ */
 enum EthProtocol : uint16_t {
     ETH_PROTO_OANAUDIO = 0x0681,
     ETH_PROTO_OANDISCO = 0x0682,
     ETH_PROTO_OANCONTROL = 0x0683
 };
 
+/**
+ * @struct LowLatHeader
+ * @brief This header contains the layer 2 header of the protocol
+ */
 struct LowLatHeader {
-    uint16_t sender_uid;
-    uint16_t dest_uid;
-    uint16_t psize;
+    uint16_t sender_uid;    /**< Sender device UID */
+    uint16_t dest_uid;      /**< Receiver device UID */
+    uint16_t psize;         /**< Packet size */
 } __attribute__((packed));
 
+/**
+ * @struct LowLatPacket
+ * @brief Layer 2 packet containing ethernet and addressing headers plus encapsulated data
+ * @tparam T Type of the encapsulated data
+ */
 template<class T>
 struct LowLatPacket {
-    ethhdr eth_header;
-    LowLatHeader llhdr;
-    T payload;
+    ethhdr eth_header;  /**< Ethernet header, linux kernel structure */
+    LowLatHeader llhdr; /**< LowLatHeader header, custom addressing scheme */
+    T payload;          /**< Encapsulated data */
 } __attribute__((packed));
 
+/**
+ * @struct INT_LLP
+ * @brief Custom-sized LowLatPacket with no raw data for encapsulated data. For internal use only
+ * @tparam payload_size__ Custom size
+ */
 template<int payload_size__>
 struct INT_LLP : public LowLatPacket<char[payload_size__]> {};
 
+/**
+ * @struct IfaceMeta
+ * @brief This struct stores physical network interface info.
+ */
 struct IfaceMeta {
-    char mac[6];
-    int idx;
+    char mac[6];   /**< MAC Address */
+    int idx;       /**< Interface ID (kernel parameter) */
 };
 
+/**
+ * Function to retreive a given network interface infos.
+ * @param name Interface name
+ * @return Interface infos if found. All zeros if not found.
+ */
 IfaceMeta get_iface_meta(const std::string& name);
 
 class NetworkMapper;
+
+/**
+ * @class LowLatSocket
+ * @brief Wrapper for raw sockets that implements a layer 2 addressing protocol.
+ */
 class LowLatSocket {
 public:
+    /**
+     * Constructor
+     * @param self_uid Host UID
+     * @param mapper Local OAN network mapper
+     */
     LowLatSocket(uint16_t self_uid, std::shared_ptr<NetworkMapper> mapper);
     ~LowLatSocket();
 
+    /**
+     * Initializes the socket
+     * @param interface Physical network interface name to attach the socket to
+     * @param proto Ethernet protocol used. @see EthProtocol
+     * @return true if initialization succeeds
+     */
     bool init_socket(std::string interface, EthProtocol proto);
 
+    /**
+     * Sends some data on the network
+     * @tparam T Sent data type
+     * @param data Pointer to the data
+     * @param dest_uid Package receiver UID
+     * @return Sent byte count. Less than 0 if error.
+     */
     template<class T>
     int send_data(const T& data, uint16_t dest_uid) {
         INT_LLP<sizeof(T)> llpck;
@@ -81,16 +131,35 @@ public:
         );
     }
 
+    /**
+     * Receive some data
+     * @tparam T Data type received
+     * @param data Pointer to the data buffer
+     * @param async This flag set the call as non-blocking if set to true
+     * @return Received byte count
+     */
     template<class T>
     int receive_data(T* data, bool async = true) {
         return recv(m_socket, data, sizeof(T), async ? MSG_DONTWAIT : 0);
     }
 
+    /**
+     * Receive raw data. @see LowLatSocket::receive_data
+     * @param data Pointer to the data buffer
+     * @param size Amount of data expected
+     * @param async This flag set the call as non-blocking if set to true
+     * @return Received byte count
+     */
     int receive_data_raw(char* data, size_t size, bool async = true) const {
         return recv(m_socket, data, size, async ? MSG_DONTWAIT : 0);
     }
 
 private:
+    /**
+     * Finds a device MAC address based on its ID.
+     * @param id ID to search for
+     * @return If found, the corresponding MAC address
+     */
     std::optional<uint64_t> get_mac(uint16_t id);
 
     sockaddr_ll m_iface_addr{};
